98ea123449a8a2a28ab638f201b6679b51635b43
pandano1 <31374047+pandano1@users.noreply.github.com>
2017-08-31 13:28:16 +0530

    Update Products.java

--- src/main/java/com/pandano1/learn/Products.java
+++ src/main/java/com/pandano1/learn/Products.java
@@ -6,7 +6,10 @@
 package com.pandano1.learn;
 
 public class Products {
-
+	static int[] noArr=  {1, 7, 3, 4};
+	static int len=noArr.length;
+	static int[] start=new int[len];
+	static int[] end=new int[len];
 	public static void main(String[] args) {
 
 		int[] noArr=  {1, 7, 3, 4};
@@ -14,24 +17,19 @@ public class Products {
 	}
 
 	private static void get_products_of_all_ints_except_at_index(int[] noArr) {
+		int j=len-1;
+		start[0]=end[j--]=1;
+		for(int i=1;i<len && j>=0;i++,j--)
+		{
+			start[i]=start[i-1]*noArr[i-1];
+			end[j]=end[j+1]*noArr[j+1];
+		}
 		System.out.print("[");
 		for(int i=0;i<noArr.length;i++)
 		{
-			System.out.print(product(noArr,i)+",");
+			System.out.print(start[i]*end[i]+",");
 		}
 		System.out.print("]");
 	}
-
-	private static int product(int[] noArr, int index) {
-		int product=1;
-		for(int i=0;i<noArr.length;i++)
-		{
-			if(i!=index)
-			{
-				product=product*noArr[i];
-			}
-		}
-		return product;
-	}
-
+	
 }
